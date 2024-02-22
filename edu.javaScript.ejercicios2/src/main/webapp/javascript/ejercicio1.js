ceacf5e6b06f965547d9047d67601f818290cfa5
DESKTOP-5TJ2NI3\csi22-mdiarro <csi22-mdiarro@colegioaltair.net>
2024-02-22 09:23:51 +0100

    Javascript

---
+++ edu.javaScript.ejercicios2/src/main/webapp/javascript/ejercicio1.js
@@ -0,0 +1,36 @@
+
+document.write("CASO 1<br>")
+var a = 5;
+var b = 10;
+
+if (a === 5) {
+  let a = 4;
+  var b = 1;
+
+  
+
+document.write(a);
+  
+
+document.write(b);
+}
+
+document.write(a);
+document.write(b);
+
+
+document.write("<br>CASO 2<br>")
+for (let i = 0; i<10; i++) {
+  
+
+document.write(i);
+}
+
+document.write(i);
+
+
+document.write("CASO 3<br>")
+    var z = 5
+    let x = z+10, y = 12;
+   
+    document.write(x + y);
